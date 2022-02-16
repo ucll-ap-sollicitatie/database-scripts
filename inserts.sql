523fe061d36368002eb26fe7598d3de3049e0fd0
ghostek99 <szymon.nidecki@student.ucll.be>
2022-02-16 13:03:35 +0100

    added insert sql file

---
+++ inserts.sql
@@ -0,0 +1,10 @@
+insert into roles (role) values('Student');
+insert into roles (role) values('Lector');
+insert into roles (role) values('Admin');
+insert into formations (formation) values('Toegepaste informatica');
+insert into formations (formation) values('Office management');
+insert into formations (formation) values('Boekhouden');
+insert into users(r_u_number, name, surname, email, role_id, formation_id) values('r0790938', 'Szymon', 'Nidecki', 'szymon.nidecki@student.ucll.be', 3, 1)
+insert into users(r_u_number, name, surname, email, role_id, formation_id) values('r0790963', 'Lorenzo', 'Catalano', 'lorenzo.catalano@student.ucll.be', 3, 1)
+insert into users(r_u_number, name, surname, email, role_id, formation_id) values('r0746926', 'Maarten', 'Van Briel', 'maarten.vanbriel@student.ucll.be', 3, 1)
+insert into users(r_u_number, name, surname, email, role_id, formation_id) values('r0709770', 'Joran', 'van Grunderbeek', 'joran.vangrunderbeek@student.ucll.be', 3, 1)
